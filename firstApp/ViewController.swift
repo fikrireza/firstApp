ffd6c8f7c6fa14df7b6e3270bf969b8b60a591c4
fikrireza <fikrirezaa@gmail.com>
2016-11-29 16:59:18 +0700

    My First iOS App

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by FRA on 11/29/16.
+//  Copyright © 2016 FRA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showMessage(){
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alertController,animated: true, completion: nil)
+    }
+    
+    @IBAction func showMessage1(){
+        let alertController = UIAlertController(title: "Opppssss, You Just Hit a Button", message: "Hi Again!!", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+
+}
+
